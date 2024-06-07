bb3ed80b168d977e19f583b61e41d842867ee2e0
No-God27 <swapunil27@gmail.com>
2024-06-07 17:36:48 +0545

    scrape

--- news.py
+++ news.py
@@ -10,13 +10,15 @@ print(response)
 if response.status_code == 200:
     soup = BeautifulSoup(response.content, 'html.parser')
     headlines = soup.find_all('h1', class_='')
-    articles = soup.find_all('p', class = 'article-summery')
+    articles = soup.find_all('p',id = 4,)
+    print('Headline:')
     for headline in headlines:
         print(headline.get_text(strip=True))
+    print('Article')
     for article in articles:
         print(article.get_text(strip=True))
 else:
-    print(f"Failed to retrieve the webpage. Status code: {response.status_code}") p
+    print("failed")
 
 
 
